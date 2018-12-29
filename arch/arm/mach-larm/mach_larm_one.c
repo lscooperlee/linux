28f3a5cb27d31b4219a417f4724be15158aac06b
Cooper <davidontech@gmail.com>
2018-12-30 00:22:48 +1300

    pass paging_init by CONFIG_ARM_PATCH_PHYS_VIRT=y and 16M aligned entry

--- arch/arm/mach-larm/mach_larm_one.c
+++ arch/arm/mach-larm/mach_larm_one.c
@@ -18,16 +18,95 @@
 
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
+#include <asm/mach/map.h>
 
-//#include <mach/serial.h>
+#define UFCON0	((volatile unsigned int *)(0xFF000000))
+
+static void larm_put(const char c) {
+    *UFCON0=c;
+}
+static void larm_puts(const char *str) {
+	while(*str){
+		larm_put(*str++);
+	};
+}
+
+void larm_print(const char *str, ...)
+{
+	char buf[256];
+	va_list ap;
+
+	va_start(ap, str);
+	vsnprintf(buf, sizeof(buf), str, ap);
+	va_end(ap);
+    
+    larm_puts(buf);
+}
+
+void larm_jump1(void) {
+}
+
+static struct map_desc larm_iodesc[] __initdata = {
+	{	/* IO space	*/
+		.virtual	= (u32)0xFF000000,
+		.pfn		= __phys_to_pfn(0xFF000000),
+		.length		= 0xF0000,
+		.type		= MT_DEVICE,
+    },
+};
+
+static void larm_l2c_write_sec(unsigned long l, unsigned u){
+    larm_print("larm_l2c_write_sec\n");
+}
+static bool larm_smp_init(void) {
+    larm_print("larm_smp_init\n");
+}
+static void larm_fixup(struct tag *t, char **s) {
+    larm_print("larm_fixup\n");
+}
+static void larm_dt_fixup(void) {
+    larm_print("larm_dt_fixup\n");
+}
+long long larm_pv_fixup(void) {
+    larm_print("larm_pv_fixup\n");
+}
+static void larm_reserve(void) {
+    larm_print("larm_reserve\n");
+}
+static void __init larm_map_io(void) {
+    iotable_init(larm_iodesc, ARRAY_SIZE(larm_iodesc));
+}
+static void larm_init_early(void) {
+    larm_print("larm_init_early\n");
+}
+static void larm_init_irq(void) {
+    larm_print("larm_init_irq\n");
+}
+static void larm_init_time(void) {
+    larm_print("larm_init_time\n");
+}
+static void larm_init_machine(void) {
+    larm_print("larm_init_machine\n");
+}
+static void larm_init_late(void) {
+    larm_print("larm_init_late\n");
+}
+static void larm_restart(enum reboot_mode r, const char *s) {
+    larm_print("larm_restart\n");
+}
+#ifdef CONFIG_GENERIC_IRQ_MULTI_HANDLER
+static void larm_handle_irq(struct pt_regs *p) {
+    larm_print("larm_handle_irq\n");
+}
+#endif
 
 MACHINE_START(LARM_ONE, "larm_one")
 	//.atag_offset = 0x100,
-	.map_io = NULL,
-	.init_early = NULL,
-	.init_irq = NULL,
-	.init_machine	= NULL,
-	.init_late	= NULL,
-	.init_time	= NULL,
-	.restart	= NULL,
+	.map_io = larm_map_io,
+	.init_early = larm_init_early,
+	.init_irq = larm_init_irq,
+	.init_machine	= larm_init_machine,
+	.init_late	= larm_init_late,
+	.init_time	= larm_init_time,
+	.restart	= larm_restart,
 MACHINE_END
